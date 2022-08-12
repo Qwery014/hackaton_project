482676c65cd7adc82f12166c84bcb831f8ccaa5a
Qwery <shamievmalik@gmail.com>
2022-08-12 15:17:29 +0600

    Project start

---
+++ src/MainRoutes.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Routes } from "react-router-dom";
+
+const MainRoutes = () => {
+  return (
+    <Routes>
+    </Routes>
+  );
+};
+
+export default MainRoutes;
