56fa607f19b238eceb9e5b57fb5d43b7375bdfac
Qwery <shamievmalik@gmail.com>
2022-08-15 01:00:17 +0600

    Footer and 2 productLists added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,57 @@
 import React from 'react';
+import "../styles/footer.css";
+import whiteLogo from "../assets/icons/logo-white.svg";
+import telegram from "../assets/icons/TG.svg";
+import vk from "../assets/icons/VK.svg";
+import instagram from "../assets/icons/INST.svg";
 
 const Footer = () => {
-    return (
-        <div>
-            
+    return <footer>
+        <div className="container">
+            <div className="footer-right">
+                <div className="footer__logo">
+                    <img src={whiteLogo} alt="" />
+                </div>
+                <ul className='footer__list'>
+                    <li className='footer__item'><a>Карта сайта</a></li>
+                    <li className='footer__item'><a>Каталог</a></li>
+                    <li className='footer__item'><a>О нас</a></li>
+                    <li className='footer__item'><a>Оплата доставка</a></li>
+                </ul>
+                <ul className='footer__list'>
+                    <li className='footer__item'><a>Опт</a></li>
+                    <li className='footer__item'><a>Акции</a></li>
+                    <li className='footer__item'><a>Блог</a></li>
+                    <li className='footer__item'><a>Контакты</a></li>
+                </ul>
+            </div>
+            <div className="footer-left">
+                <div className="footer-rules">
+                    <p>Пользовательское соглашение</p>
+                    <p>Политика конфиденциальности</p>
+                </div>
+                <div className="footer-share">
+                    <ul>
+                        <li>
+                            <a>
+                                <img src={vk} alt="" />
+                            </a>
+                        </li>
+                        <li>
+                            <a>
+                                <img src={telegram} alt="" />
+                            </a>
+                        </li>
+                        <li>
+                            <a>
+                                <img src={instagram} alt="" />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-    );
+    </footer>;
 };
 
 export default Footer;
--- src/components/homeRecommendations/Bestseller.jsx
+++ src/components/homeRecommendations/Bestseller.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ProductCard from '../products/ProductCard';
 import "../../styles/productCard.css"
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Bestseller = () => {
-    return (
-        <div className='bestseller'>
-            <h2>
-                ХИТЫ ПРОДАЖ
-            </h2>
-            <div className='ProductRecommendation'>
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-            </div>
+    useEffect(() => {
+        AOS.init();
+        AOS.refresh();
+    }, []);
+    return <div className='bestseller' data-aos="fade-down" data-aos-once="true" data-aos-duration="1100">
+        <h2>
+            ХИТЫ ПРОДАЖ
+        </h2>
+        <div className='ProductRecommendation'>
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
         </div>
-    );
+    </div>;
 };
 
 export default Bestseller;
