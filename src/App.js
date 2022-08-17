619cfa2a0894df85d99b495d30970e293d09e39a
islam030804 <globus.user95@gmail.com>
2022-08-17 15:32:51 +0600

    CartPage added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from "react"
+import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MainRoutes from "./MainRoutes";
 
 function App() {
-  return <>
-    <Header />
-    <MainRoutes />
-    <Footer />
-  </>;
+  return (
+    <>
+      <Header />
+      <MainRoutes />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
